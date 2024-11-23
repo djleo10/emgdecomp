1de5a2f3d5fbb60cdd5350ee09a8a41d02660367
djleo10 <79630110+djleo10@users.noreply.github.com>
2024-11-23 17:11:51 -0500

    decomposition.py

--- emgdecomp/decomposition.py
+++ emgdecomp/decomposition.py
@@ -842,7 +842,7 @@ class EmgDecomposition(object):
 
     def _firings_dtype(self):
         return np.dtype([
-            ('source_idx', np.int),
+            ('source_idx', np.int64),
             ('discharge_samples', np.int),
             ('discharge_seconds', np.float),
             ('squared_amplitude', np.float),
